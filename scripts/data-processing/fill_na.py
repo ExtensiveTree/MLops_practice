d3c2c7ca45e6506c0383279d26b7a4a2d1f07f2c
akashy <jen.danilow2013@yandex.ru>
2023-11-09 21:31:42 +0500

    Update fill_na

--- scripts/data-processing/fill_na.py
+++ scripts/data-processing/fill_na.py
@@ -4,7 +4,7 @@ import io
 
 if len(sys.argv) != 2:
     sys.stderr.write("Arguments error. Usage:\n")
-    sys.stderr.write("\tpython get_features.py data-file\n")
+    sys.stderr.write("\tpython fil_na.py data-file\n")
     sys.exit(1)
 f_input = sys.argv[1]
 f_output = os.path.join("data", "stage2", "train.csv")
